0ef48e2945b91ad736ccffa7693475b3240cf32e
Saifudeen369 <saifudheen232003@gmail.com>
2024-10-01 11:21:18 +0530

    Add files via upload

---
+++ area of shapes.py
@@ -0,0 +1,12 @@
+pi=3.14
+r=float(input("enter the radius"))
+area=pi*r*r
+print("area of circle is",area)
+l=int(input("enter length"))
+b=int(input("enter breadth"))
+Re=l*b
+print("area of rectangle is",Re)
+c=int(input("enter the base"))
+d=int(input("enter the height"))
+t=0.5*c*d
+print("area of triangle",t) 
