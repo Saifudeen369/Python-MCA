dc1ecef06991e279ab4f48b694b2ddca789e7850
Saifudeen369 <saifudheen232003@gmail.com>
2024-10-01 11:44:03 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,12 @@
+print("enter marks of five subjects: ")
+S1=float(input())
+S2=float(input())
+S3=float(input())
+S4=float(input())
+S5=float(input())
+total=S1+S2+S3+S4+S5;
+average=total/5.0;
+per=(total/500.0)*100;
+print("Total marks= ",total)
+print("average marks= ",average)
+print("percentage= ",per)
